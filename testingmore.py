758727688cb93a74a000f14ead210d6230d46cc1
blanklogan <officiallogandylan@gmail.com>
2026-04-19 23:01:31 -0700

    Add files via upload

---
+++ testingmore.py
@@ -0,0 +1,36 @@
+
+operation = input("Would you like to add, subtract, multiply, or divide? ")
+
+if operation == "add":
+    number1_input = input("Enter your first number. ")
+    number2_input = input("Enter your second number. ")
+    number1 = int(number1_input)
+    number2 = int(number2_input)
+    result = number1 + number2
+    print (f"Answer = {result}")
+    
+if operation == "subtract":
+    number1_input = input("Enter your first number. ")
+    number2_input = input("Enter your second number. ")
+    number1 = int(number1_input)
+    number2 = int(number2_input)
+    result = number1 - number2
+    print (f"Answer = {result}")
+
+if operation == "multiply":
+    number1_input = input("Enter your first number. ")
+    number2_input = input("Enter your second number. ")
+    number1 = int(number1_input)
+    number2 = int(number2_input)
+    result = number1 * number2
+    print (f"Answer = {result}")
+
+if operation == "divide":
+    number1_input = input("Enter your first number. ")
+    number2_input = input("Enter your second number. ")
+    number1 = int(number1_input)
+    number2 = int(number2_input)
+    result = number1 / number2
+    print (f"Answer = {result}")
+
+
