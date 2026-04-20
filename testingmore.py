ef5764efb99c484ddeca93355e74afc40673e2cc
blanklogan <officiallogandylan@gmail.com>
2026-04-19 23:16:52 -0700

    Updating calculator

--- testingmore.py
+++ testingmore.py
@@ -1,36 +1,37 @@
 
-operation = input("Would you like to add, subtract, multiply, or divide? ")
-
-if operation == "add":
-    number1_input = input("Enter your first number. ")
-    number2_input = input("Enter your second number. ")
-    number1 = int(number1_input)
-    number2 = int(number2_input)
-    result = number1 + number2
-    print (f"Answer = {result}")
-    
-if operation == "subtract":
-    number1_input = input("Enter your first number. ")
-    number2_input = input("Enter your second number. ")
-    number1 = int(number1_input)
-    number2 = int(number2_input)
-    result = number1 - number2
-    print (f"Answer = {result}")
-
-if operation == "multiply":
-    number1_input = input("Enter your first number. ")
-    number2_input = input("Enter your second number. ")
-    number1 = int(number1_input)
-    number2 = int(number2_input)
-    result = number1 * number2
-    print (f"Answer = {result}")
-
-if operation == "divide":
-    number1_input = input("Enter your first number. ")
-    number2_input = input("Enter your second number. ")
-    number1 = int(number1_input)
-    number2 = int(number2_input)
-    result = number1 / number2
-    print (f"Answer = {result}")
-
-
+while True:
+    operation = input("Would you like to add, subtract, multiply, or divide? ")
+    if operation == "add":
+        number1_input = input("Enter your first number. ")
+        number2_input = input("Enter your second number. ")
+        number1 = int(number1_input)
+        number2 = int(number2_input)
+        result = number1 + number2
+        print (f"Answer = {result}")  
+        break
+    if operation == "subtract":
+        number1_input = input("Enter your first number. ")
+        number2_input = input("Enter your second number. ")
+        number1 = int(number1_input)
+        number2 = int(number2_input)
+        result = number1 - number2
+        print (f"Answer = {result}")
+        break
+    if operation == "multiply":
+        number1_input = input("Enter your first number. ")
+        number2_input = input("Enter your second number. ")
+        number1 = int(number1_input)
+        number2 = int(number2_input)
+        result = number1 * number2
+        print (f"Answer = {result}")
+        break
+    if operation == "divide":
+        number1_input = input("Enter your first number. ")
+        number2_input = input("Enter your second number. ")
+        number1 = int(number1_input)
+        number2 = int(number2_input)
+        result = number1 / number2
+        print (f"Answer = {result}")
+        break
+    if operation != "add" or "subtract" or "multiply" or "divide":
+        print("Try again") 
